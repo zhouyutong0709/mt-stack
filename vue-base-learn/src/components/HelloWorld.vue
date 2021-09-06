0f97c575bc1fe97d9be9497fffd47788527f4b9a
v_ytongzhou <v_ytongzhou@tencent.com>
2021-09-06 18:06:45 +0800

    vue基础之自定义指令和计算属性

---
+++ vue-base-learn/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <!-- 自定义指令 -->
+    <div v-n="4"></div>
+
+    <!-- 计算属性 -->
+    <!-- 应用场景-具有依赖关系的数据监听 -->
+    <div :class="[a1, a2]">
+      借钱{{ totalMoney }},还钱{{ borrowMonry }},还剩{{ results }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      totalMoney: 1000,
+      borrowMonry: 100
+    };
+  },
+  computed: {
+    results: function() {
+      return this.totalMoney - this.borrowMonry;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
